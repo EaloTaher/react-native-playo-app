3f3d89073d12791c1f6390c5cfe76a102fe9de55
Ealam Taher <ealozaher@gmail.com>
2024-10-02 13:04:22 +0300

    added the backend models

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const moment = require("moment");
+
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const jwt = require("jsonwebtoken");
+
+mongoose
+  .connect(
+    "mongodb+srv://ealozaher:1abN5qSYwjPi0hhh@playoapp.mtmiw.mongodb.net/?retryWrites=true&w=majority&appName=PlayoApp"
+  )
+  .then(() => console.log("Connected to mongoDB"))
+  .catch((err) => console.log("Error connecting to MongoDB", err));
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
